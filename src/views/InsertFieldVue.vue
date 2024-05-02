94664d696a1da0d5009ab72fb8e794546709c5a7
Rithiya Vibol <vibolrithiya@gmail.com>
2024-05-02 22:56:40 +0700

    Component and computed review lesson.

---
+++ src/views/InsertFieldVue.vue
@@ -0,0 +1,22 @@
+<template>
+  <button @click="insertNameIntoList">Press me</button>
+  <input v-model="input" placeholder="Enter me" />
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const input = ref(null)
+
+const emit = defineEmits(['InsertName'])
+
+function insertNameIntoList() {
+  //   const rise = {
+  //     name: input.value
+  //   }
+  //   data.value.push(rise)
+
+//   console.log(input.value)      
+    emit("InsertName", input.value)
+}
+</script>
---
+++ src/views/ListStudentName.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="decor"> {{ no }}. {{ props.name }}</div>
+</template>
+
+<script setup>
+    const props= defineProps(['name', 'no']);
+</script>
+
+<style scoped>
+.decor {
+  color: black;
+  padding: 0 50px;
+  background-color: bisque;
+  border: 2px solid transparent;
+  border-radius: 90px;
+  margin-bottom: 10px;
+}
+</style>
